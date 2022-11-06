860a3b51a20f5777d0b41401d1bde78f1ebbed04
Ivan <ivanbrazas@gmail.com>
2022-11-06 12:50:13 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,73 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
+import api from "./api"
 
 function App() {
-  const answer = "RIGHT";
+  const [answer, setAnswer] = useState("")
   const [turn, setTurn] = useState<number>(0);
   const [status, setStatus] = useState<"playing" | "finished">("playing");
   const [words, setWords] = useState<string[][]>(() =>
     Array.from({length: 6}, () => new Array(5).fill("")),
   );
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (status === "playing") {
-      switch (event.key) {
-        case "Enter": {
-          if (words[turn].some((letter) => letter === "")) {
+  const handleKeyDown =  useCallback(
+    (event: KeyboardEvent) => {
+      if (status === "playing") {
+        switch (event.key) {
+          case "Enter": {
+            if (words[turn].some((letter) => letter === "")) {
+              return;
+            }
+  
+            if (words[turn].join("") === answer) {
+              setStatus("finished");
+              
+              setTimeout(() => {
+                setWords(    Array.from({length: 6}, () => new Array(5).fill("")))
+                setTurn(0)
+                setStatus("playing")
+              }, 1000)
+            }
+  
+            setTurn((turn) => turn + 1);
+  
             return;
           }
-
-          if (words[turn].join("") === answer) {
-            setStatus("finished");
-          }
-
-          setTurn((turn) => turn + 1);
-
-          return;
-        }
-        case "Backspace": {
-          let firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
-
-          if (firstEmptyIndex === -1) {
-            firstEmptyIndex = words[turn].length;
-          }
-
-          words[turn][firstEmptyIndex - 1] = "";
-
-          setWords(words.slice());
-
-          return;
-        }
-        default: {
-          if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
-            const firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
-
-            if (firstEmptyIndex === -1) return;
-
-            words[turn][firstEmptyIndex] = event.key.toUpperCase();
-
+          case "Backspace": {
+            let firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
+  
+            if (firstEmptyIndex === -1) {
+              firstEmptyIndex = words[turn].length;
+            }
+  
+            words[turn][firstEmptyIndex - 1] = "";
+  
             setWords(words.slice());
-
+  
             return;
+          }
+          default: {
+            if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
+              const firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
+  
+              if (firstEmptyIndex === -1) return;
+  
+              words[turn][firstEmptyIndex] = event.key.toUpperCase();
+  
+              setWords(words.slice());
+  
+              return;
+            }
           }
         }
       }
-    }
-  };
+
+
+  }, [turn, status, words, answer]) 
+
+  useEffect(() => {
+    api.word.random().then(setAnswer)
+  }, [])
+
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
@@ -66,10 +81,10 @@ function App() {
         <section className="word">
           {word.map((letter, letterIndex) => {
             const isCorrect = letter && wordIndex < turn && letter === answer[letterIndex];
-            const isPresent = false;
+            const isPresent = answer.toLowerCase().split("").includes(letter.toLowerCase()) ? true : false;
 
             return (
-              <article className={`letter ${isPresent && "present"} ${isCorrect && "correct"}`}>
+              <article key={letterIndex} className={`letter ${isPresent && "present"} ${isCorrect && "correct"}`}>
                 {letter}
               </article>
             );
